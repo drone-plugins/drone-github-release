feb4d189be9aaae90f5e1fe69cdfe3ee6815b47a
Don <don.j.olmstead@gmail.com>
2020-06-02 09:25:56 -0700

    Fix error string lint

--- plugin/release.go
+++ plugin/release.go
@@ -43,7 +43,7 @@ func (rc *releaseClient) buildRelease() (*github.RepositoryRelease, error) {
 	}
 
 	if err != nil {
-		return nil, fmt.Errorf("Failed to retrieve or create a release: %s", err)
+		return nil, fmt.Errorf("failed to retrieve or create a release: %w", err)
 	}
 
 	return release, nil
@@ -53,7 +53,7 @@ func (rc *releaseClient) getRelease() (*github.RepositoryRelease, error) {
 	release, _, err := rc.Client.Repositories.GetReleaseByTag(rc.Context, rc.Owner, rc.Repo, rc.Tag)
 
 	if err != nil {
-		return nil, fmt.Errorf("Release %s not found", rc.Tag)
+		return nil, fmt.Errorf("release %s not found", rc.Tag)
 	}
 
 	fmt.Printf("Successfully retrieved %s release\n", rc.Tag)
@@ -69,7 +69,7 @@ func (rc *releaseClient) editRelease(rid int64) (*github.RepositoryRelease, erro
 	release, _, err := rc.Client.Repositories.EditRelease(rc.Context, rc.Owner, rc.Repo, rid, rr)
 
 	if err != nil {
-		return nil, fmt.Errorf("Failed to update release: %s", err)
+		return nil, fmt.Errorf("failed to update release: %w", err)
 	}
 
 	fmt.Printf("Successfully updated %s release\n", rc.Tag)
@@ -88,7 +88,7 @@ func (rc *releaseClient) newRelease() (*github.RepositoryRelease, error) {
 	release, _, err := rc.Client.Repositories.CreateRelease(rc.Context, rc.Owner, rc.Repo, rr)
 
 	if err != nil {
-		return nil, fmt.Errorf("Failed to create release: %s", err)
+		return nil, fmt.Errorf("failed to create release: %w", err)
 	}
 
 	fmt.Printf("Successfully created %s release\n", rc.Tag)
@@ -99,7 +99,7 @@ func (rc *releaseClient) uploadFiles(id int64, files []string) error {
 	assets, _, err := rc.Client.Repositories.ListReleaseAssets(rc.Context, rc.Owner, rc.Repo, id, &github.ListOptions{})
 
 	if err != nil {
-		return fmt.Errorf("Failed to fetch existing assets: %s", err)
+		return fmt.Errorf("failed to fetch existing assets: %w", err)
 	}
 
 	var uploadFiles []string
@@ -112,12 +112,12 @@ files:
 				case "overwrite":
 					// do nothing
 				case "fail":
-					return fmt.Errorf("Asset file %s already exists", path.Base(file))
+					return fmt.Errorf("asset file %s already exists", path.Base(file))
 				case "skip":
 					fmt.Printf("Skipping pre-existing %s artifact\n", *asset.Name)
 					continue files
 				default:
-					return fmt.Errorf("Internal error, unkown file_exist value %s", rc.FileExists)
+					return fmt.Errorf("internal error, unknown file_exist value %s", rc.FileExists)
 				}
 			}
 		}
@@ -129,13 +129,13 @@ files:
 		handle, err := os.Open(file)
 
 		if err != nil {
-			return fmt.Errorf("Failed to read %s artifact: %s", file, err)
+			return fmt.Errorf("failed to read %s artifact: %w", file, err)
 		}
 
 		for _, asset := range assets {
 			if *asset.Name == path.Base(file) {
 				if _, err := rc.Client.Repositories.DeleteReleaseAsset(rc.Context, rc.Owner, rc.Repo, *asset.ID); err != nil {
-					return fmt.Errorf("Failed to delete %s artifact: %s", file, err)
+					return fmt.Errorf("failed to delete %s artifact: %w", file, err)
 				}
 
 				fmt.Printf("Successfully deleted old %s artifact\n", *asset.Name)
@@ -145,7 +145,7 @@ files:
 		uo := &github.UploadOptions{Name: path.Base(file)}
 
 		if _, _, err = rc.Client.Repositories.UploadReleaseAsset(rc.Context, rc.Owner, rc.Repo, id, uo, handle); err != nil {
-			return fmt.Errorf("Failed to upload %s artifact: %s", file, err)
+			return fmt.Errorf("failed to upload %s artifact: %w", file, err)
 		}
 
 		fmt.Printf("Successfully uploaded %s artifact\n", file)
