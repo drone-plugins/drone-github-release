68bb9b529697cfee65f161891eef803e46ba6c6a
Don <don.j.olmstead@gmail.com>
2021-04-14 07:18:18 -0700

    Update go-github to v35

--- plugin/release.go
+++ plugin/release.go
@@ -11,7 +11,7 @@ import (
 	"os"
 	"path"
 
-	"github.com/google/go-github/v34/github"
+	"github.com/google/go-github/v35/github"
 )
 
 // Release holds ties the drone env data and github client together.
