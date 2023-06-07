edc6f5b36433d586de95c1229cc9280335ad8619
Don Olmstead <don.j.olmstead@gmail.com>
2023-06-07 11:02:31 -0700

    Update dependencies
    Manually update the go-github dependency and just use `go mod tidy` for the rest.

--- plugin/release.go
+++ plugin/release.go
@@ -11,7 +11,7 @@ import (
 	"os"
 	"path"
 
-	"github.com/google/go-github/v50/github"
+	"github.com/google/go-github/v53/github"
 )
 
 // Release holds ties the drone env data and github client together.
